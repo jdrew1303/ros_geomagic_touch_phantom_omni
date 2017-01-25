09fe894948b13000a6043af5a895d5e7ba85689f
Bruno Guilhermo de Barros Valerio <bruno.gbv@poli.ufrj.br>
2017-01-25 12:32:02 -0200

    Program working, cleaning needed.
    Signed-off-by: Bruno Guilhermo de Barros Valerio <bruno.gbv@poli.ufrj.br>

---
+++ omni_driver/include/omnibase.h
@@ -0,0 +1,277 @@
+#pragma once
+
+
+#include <vector>
+
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+#include <geometry_msgs/Vector3.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include "omni_driver/OmniButtonEvent.h"
+
+class OmniBase
+{
+private:
+    boost::shared_mutex mutex_state;
+
+protected:
+    typedef boost::posix_time::microsec_clock Clock;
+    typedef boost::posix_time::ptime Time;
+    typedef boost::posix_time::time_duration TimeDuration;
+
+    struct OmniState
+    {
+        std::vector<double> angles_docked;
+        std::vector<double> angles;
+        std::vector<double> angles_last;
+        std::vector<bool>   buttons;
+        std::vector<double> control;
+        std::vector<double> force;
+        std::vector<double> position;
+        std::vector<double> orientation;
+        std::vector<double> velocities;
+
+        bool control_on = false;
+        bool calibrated = false;
+
+        unsigned int seq = 0;
+        Time stamp;
+
+        OmniState()
+        {
+            // Joints
+            angles_docked.resize(6);
+            angles.resize(6);
+            angles_last.resize(6);
+            velocities.resize(6);
+
+            // Buttons
+            buttons.resize(2);
+
+            // Others
+            control.resize(3);
+            force.resize(3);
+            position.resize(3);
+            orientation.resize(4);
+        }
+    };
+
+protected:
+    OmniState state;
+
+    std::string name;
+    std::string topic_name;
+
+    ros::NodeHandlePtr node;
+
+    ros::Subscriber sub_torque;
+
+    ros::Publisher pub_joint;
+    sensor_msgs::JointState joint_state;
+
+    ros::Publisher pub_pose;
+    geometry_msgs::PoseStamped pose_stamped;
+
+    ros::Publisher pub_button;
+    omni_driver::OmniButtonEvent button_event;
+
+    ros::Subscriber sub_enable_control;
+
+    bool last_buttons[2];
+
+    bool enable_force_flag;
+
+protected:
+    typedef boost::unique_lock<boost::shared_mutex>            LockUnique;
+    typedef boost::shared_lock<boost::shared_mutex>            LockShared;
+    typedef boost::upgrade_lock<boost::shared_mutex>           LockUpgrade;
+    typedef boost::upgrade_to_unique_lock<boost::shared_mutex> LockUpgradeToUnique;
+
+    /**
+     * @brief Gets the mutex used for accessing the robot's state.
+     * @return The mutex.
+     */
+    inline boost::shared_mutex & getStateMutex()
+    {
+        return mutex_state;
+    }
+
+    /**
+     * @brief Gets the robot's state.
+     * @return The robot's state.
+     */
+    inline OmniState * getState()
+    {
+        return &state;
+    }
+
+public:
+    explicit OmniBase(const std::string &name = "");
+
+    /**
+     * @brief Publishes the current robot's state.
+     */
+    void publishOmniState();
+
+    /**
+     * @brief Attempts to connect to the robot.
+     * @return True if the connection succeeds. False otherwise.
+     */
+    virtual bool connect() = 0;
+
+    /**
+     * @brief Attempts to close the connection to the robot.
+     * @return True if the connection was closed. False otherwise.
+     */
+    virtual void disconnect() = 0;
+
+    /**
+     * @brief Checks if the device is connected.
+     * @return True if connected. False otherwise.
+     * @see connect, disconnect
+     */
+    virtual bool connected() = 0;
+
+    /**
+     * @brief Gets the current joint angles.
+     * @param angles Vector that will store the angles.
+     */
+    inline void getJointAngles(std::vector<double> &angles)
+    {
+        // Protect the critical section.
+        LockShared lock( getStateMutex() );
+        angles = state.angles;
+    }
+
+    /**
+     * @brief Gets the current buttons' state.
+     * @param button Vector that will store the states.
+     */
+    void getButtonsState(std::vector<bool>& button)
+    {
+        // Protect the critical section.
+        LockShared lock( getStateMutex() );
+        button = state.buttons;
+    }
+
+    /**
+     * @brief Gets the current force acting on the tip.
+     * @param force Vector that will store the force.
+     */
+    inline void getForce(std::vector<double> &force)
+    {
+        LockShared lock( getStateMutex() );
+        force = state.force;
+    }
+
+    /**
+     * @brief Sets the torque on the first three joints.
+     * @param torque 3-elements vector with the torque values.
+     */
+    inline void setTorque(const std::vector<double> &torque)
+    {
+        LockUnique lock( getStateMutex() );
+        state.control[0] = (torque[0] + 1) / 2 * 4095;
+        state.control[1] = (torque[1] + 1) / 2 * 4095;
+        state.control[2] = (torque[2] + 1) / 2 * 4095;
+    }
+
+    /**
+     * @brief Resets the torque on the first three joints.
+     * @see setTorque
+     */
+    inline void resetTorque()
+    {
+        std::vector<double> null_torque(3,0);
+        this->setTorque(null_torque);
+    }
+
+    /**
+     * @brief Chooses if the control on the first three joints is on.
+     * @param enable True to enable. False otherwise.
+     */
+    inline void enableControl(bool enable)
+    {
+        {
+            // Creating a new scope to avoid a deadlock
+            LockUnique lock( getStateMutex() );
+            state.control_on = enable;
+            enable_force_flag = enable;
+        }
+        this->resetTorque();
+    }
+
+    /**
+     * @brief Gets the current joint velocities.
+     * @param vel Vector that will store the velocities.
+     */
+    inline void getJointVelocities(std::vector<double> &vel)
+    {
+        LockShared lock( getStateMutex() );
+        vel = state.velocities;
+    }
+
+    /**
+     * @brief Gets the current tip position with respect to the robot's base frame.
+     * @param pos Vector that will store the position.
+     */
+    inline void getTipPosition(std::vector<double> &pos)
+    {
+        LockShared lock( getStateMutex() );
+        pos = state.position;
+    }
+
+    /**
+     * @brief Gets the current tip orientation with respect to the robot's base frame.
+     * @param ori Vector that will store the orientation as a quaternion.
+     */
+    inline void getTipOrientation(std::vector<double> &ori)
+    {
+        LockShared lock( getStateMutex() );
+        ori = state.orientation;
+    }
+
+    /**
+     * @brief Gets the current tip position and orientation with respect to the robot's base frame.
+     * @param pos Vector that will store the position.
+     * @param ori Vector that will store the orientation as a quaternion.
+     * @see getTipPosition, getTipOrientation
+     */
+    inline void getTipPose(std::vector<double> &pos, std::vector<double> &ori)
+    {
+        LockShared lock( getStateMutex() );
+        pos = state.position;
+        ori = state.orientation;
+    }
+
+    /**
+     * @brief Gets the omni name param.
+     * @return Returns the robot's name.
+     */
+    inline std::string getTopicName()
+    {
+        return this->name;
+    }
+
+    /**
+     * @brief Checks if the device has been calibrated.
+     * @return True if calibrated. False otherwise.
+     */
+    inline bool calibrated()
+    {
+        LockShared lock( getStateMutex() );
+        return state.calibrated;
+    }
+
+// ROS Callbacks
+public:
+    void torqueCallback(const geometry_msgs::Vector3::ConstPtr& msg);
+    void enableControlCallback(const std_msgs::Bool::ConstPtr& msg);
+};
+
+typedef boost::shared_ptr<OmniBase> OmniBasePtr;
---
+++ omni_driver/src/omnifirewire.cpp
@@ -0,0 +1,503 @@
+#include "../include/omnifirewire.h"
+
+#include <boost/bind.hpp>
+
+
+OmniFirewire::OmniFirewire(const std::string &serial_number, const std::string &name)
+    : OmniBase(name),
+    thread_status(THREAD_READY),
+    serial_number_(serial_number),
+    handle_(NULL), tx_handle_(NULL), rx_handle_(NULL),
+    port_(-1), node_(-1), tx_iso_channel_(-1), rx_iso_channel_(-1)
+{
+
+    // Initialize the variables.
+    std::memset(current_joint_angles_, 0, sizeof(current_joint_angles_));
+    std::memset(home_joint_angles_, 0, sizeof(home_joint_angles_));
+    std::memset(current_buttons_, 0, sizeof(current_buttons_));
+    //
+    this->resetTorque();
+}
+
+OmniFirewire::~OmniFirewire()
+{
+    this->disconnect();
+}
+
+void OmniFirewire::callback(OmniBase::OmniState *state)
+{
+}
+
+enum raw1394_iso_disposition OmniFirewire::callbackWrite(raw1394handle_t handle, unsigned char *data, unsigned int* len, unsigned char *tag, unsigned char *sy, int, unsigned int)
+{
+    *len = 0;
+    *tag = 0;
+    *sy = 0;
+    // Get the OmniFirewire object for this handle.
+    OmniFirewire *omni = reinterpret_cast<OmniFirewire*>(raw1394_get_userdata(handle));
+    if (omni == NULL)
+    {
+        return RAW1394_ISO_OK;
+    }
+
+    // Edit force output
+    {
+        OmniBase::LockShared lock( omni->getStateMutex() );
+        if (omni->state.control_on)
+        {
+            omni->tx_iso_buffer_.force_x = omni->state.control[0];
+            omni->tx_iso_buffer_.force_y = omni->state.control[1];
+            omni->tx_iso_buffer_.force_z = omni->state.control[2];
+        }
+        else
+        {
+            omni->tx_iso_buffer_.status.force_enabled = 0;
+        }
+    }
+
+    if (omni->enable_force_flag)
+    {
+        if (omni->tx_iso_buffer_.status.force_enabled == 1)
+            omni->tx_iso_buffer_.status.force_enabled = 0;
+        else
+        {
+            omni->tx_iso_buffer_.status.force_enabled = 1;
+            omni->enable_force_flag = false;
+        }
+    }
+
+    // Copy data.
+    std::memcpy(data, &omni->tx_iso_buffer_, sizeof(omni->tx_iso_buffer_));
+    *len = sizeof(omni->tx_iso_buffer_);
+    *tag = 0;
+    *sy = 0;
+    return RAW1394_ISO_OK;
+}
+
+enum raw1394_iso_disposition OmniFirewire::callbackRead(raw1394handle_t handle, unsigned char* data, unsigned int len, unsigned char channel, unsigned char tag, unsigned char sy, unsigned int cycle, unsigned int dropped)
+{
+    // Get the OmniFirewire object for this handle.
+    OmniFirewire* omni = reinterpret_cast<OmniFirewire*>(raw1394_get_userdata(handle));
+    if (omni == NULL)
+    {
+        return RAW1394_ISO_OK;
+    }
+
+    const OmniFirewire::RxIsoBuffer* newbuf = reinterpret_cast<OmniFirewire::RxIsoBuffer*>(data);
+
+    OmniBase::LockUnique lock( omni->getStateMutex() );
+
+    // Calibrate if docked.
+    if (!newbuf->status.undocked)
+    {
+        // Calibrate.
+        omni->state.angles_docked[0] = 2.0 * M_PI *  newbuf->encoder_x / 15000.0;
+        omni->state.angles_docked[1] = 2.0 * M_PI * -newbuf->encoder_y / 15000.0 - 0.23;
+        omni->state.angles_docked[2] = 2.0 * M_PI *  newbuf->encoder_z / 15000.0 + 0.37;
+        // Calibrated: the dock LED should be always on.
+        omni->tx_iso_buffer_.status.dock_led0 = 1;
+        omni->tx_iso_buffer_.status.dock_led1 = 1;
+        omni->state.calibrated = true;
+    }
+    // Compute the encoder values.
+    omni->state.angles[0] = 2.0 * M_PI
+            *  newbuf->encoder_x / 15000.0 - omni->state.angles_docked[0];
+
+    omni->state.angles[1] = 2.0 * M_PI
+            * -newbuf->encoder_y / 15000.0 - omni->state.angles_docked[1];
+
+    omni->state.angles[2] = 2.0 * M_PI
+            *  newbuf->encoder_z / 15000.0 - omni->state.angles_docked[2]
+            - omni->state.angles[1];
+
+    // Add the current gimbal pot value for filtering.
+    omni->pot_filter_accum_[0] += (double)newbuf->gimbal_a_x
+            / (newbuf->gimbal_a_x + newbuf->gimbal_b_x);
+
+    omni->pot_filter_accum_[1] += (double)newbuf->gimbal_a_y
+            / (newbuf->gimbal_a_y + newbuf->gimbal_b_y);
+
+    omni->pot_filter_accum_[2] += (double)newbuf->gimbal_a_z
+            / (newbuf->gimbal_a_z + newbuf->gimbal_b_z);
+
+    if (--omni->pot_filter_count_ <= 0)
+    {
+        omni->pot_filter_count_ = RAW_OMNI_POT_FILTER_TAPS;
+
+        // Normalize.
+        omni->pot_filter_accum_[0] /= RAW_OMNI_POT_FILTER_TAPS;
+        omni->pot_filter_accum_[1] /= RAW_OMNI_POT_FILTER_TAPS;
+        omni->pot_filter_accum_[2] /= RAW_OMNI_POT_FILTER_TAPS;
+
+        // Compute the gimbal values.
+        omni->state.angles[3]
+                =  5.48 * omni->pot_filter_accum_[0] - 2.74;
+        omni->state.angles[4]
+                = -5.28 * omni->pot_filter_accum_[1] + 2.64;
+        omni->state.angles[5]
+                =  4.76 * omni->pot_filter_accum_[2] - 2.38;
+
+        // Reset the accumulator array.
+        omni->pot_filter_accum_[0] = 0.0;
+        omni->pot_filter_accum_[1] = 0.0;
+        omni->pot_filter_accum_[2] = 0.0;
+    }
+
+    // Get the button state.
+    omni->state.buttons[0] = !newbuf->status.button1;
+    omni->state.buttons[1] = !newbuf->status.button2;
+
+    return RAW1394_ISO_OK;
+}
+
+
+bool OmniFirewire::connect()
+{
+    // Make sure the device is disconnected at this point.
+    disconnect();
+
+    // Find the Phantom Omni.
+    const std::vector<OmniInfo>& omnis = enumerate_omnis();
+    typedef std::vector<OmniInfo>::const_iterator CIT;
+    for (CIT it = omnis.begin(); it != omnis.end(); ++it) {
+        if (it->serial == serial_number_) {
+            // Serial number matched: found the omni!
+            port_ = it->port;
+            node_ = it->node;
+
+            break;
+        }
+    }
+    if (node_ == -1) {
+        // Failed to find the omni.
+        return false;
+    }
+
+    // Create a 1394 handle.
+    handle_ = raw1394_new_handle();
+    raw1394_set_userdata(handle_, this);
+
+    // Set the port.
+    if (raw1394_set_port(handle_, port_) == -1) {
+        return false;
+    }
+
+    // Initialize the Tx buffer.
+    tx_iso_buffer_.force_x     = 0x07ff;
+    tx_iso_buffer_.force_y     = 0x07ff;
+    tx_iso_buffer_.force_z     = 0x07ff;
+    tx_iso_buffer_.status.bits = 0x53c0;
+    tx_iso_buffer_.padding1    = 0x0000;
+    tx_iso_buffer_.padding2    = 0x0000;
+    tx_iso_buffer_.status.dock_led0 = 0;
+    tx_iso_buffer_.status.dock_led1 = 1;
+    tx_iso_buffer_.status.force_enabled = 1;
+
+    // Reset the pot filter counter.
+    pot_filter_accum_[0] = 0.0;
+    pot_filter_accum_[1] = 0.0;
+    pot_filter_accum_[2] = 0.0;
+    pot_filter_count_ = RAW_OMNI_POT_FILTER_TAPS;
+
+    // Start isochronous transmission.
+    if (!startIsochronousTransmission()) {
+        ROS_ERROR("Failed to start isochronous transmission!");
+        return false;
+    }
+
+    ROS_INFO("Isochronous transmission started successfully.");
+    return true;
+}
+
+
+void OmniFirewire::disconnect()
+{
+    // Turn the dock LED off.
+    tx_iso_buffer_.status.dock_led0 = 0;
+    tx_iso_buffer_.status.dock_led1 = 0;
+
+    {
+        OmniBase::LockUnique lock( getStateMutex() );
+        this->state.calibrated = false;
+    }
+
+    // Stop isochronous data transmission.
+    stopIsochronousTransmission();
+
+    port_ = -1;
+    node_ = -1;
+
+    if ( connected() )
+    {
+        raw1394_destroy_handle(handle_);
+        handle_ = NULL;
+    }
+
+}
+
+bool OmniFirewire::connected()
+{
+    return handle_ != NULL;
+}
+
+bool OmniFirewire::run()
+{
+    // Open Phantom Omni.
+    if (!this->connect()) {
+        ROS_ERROR("Failed to open Phantom Omni.");
+        return false;
+    }
+    ROS_INFO("%s (%s): opened.", this->name.c_str(), this->serial_number_.c_str());
+
+    // Spin.
+    ros::spin();
+
+    return true;
+}
+
+uint32_t OmniFirewire::pack_serial_number(const std::string& unpacked)
+{
+    // Serial number must have 11 characters.
+    if (unpacked.size() != 11) {
+        // Invalid length.
+        return 0;
+    }
+
+    // Extract digits into 5 integers.
+    uint32_t a, b, c, d, e;
+    int filled = std::sscanf(unpacked.c_str(),
+                             "%1u%2u%2u%1u%5u", &a, &b, &c, &d, &e);
+    if (filled != 5) {
+        // Extraction failed.
+        return 0;
+    }
+
+    uint32_t packed = 0x80000001;
+    packed |= ((a & 0x007) << 23);  // a maps to Bit 25..23.
+    packed |= ((b & 0x00f) << 19);  // b maps to Bit 22..19.
+    packed |= ((c & 0x01f) << 14);  // c maps to Bit 18..14.
+    packed |= ((d & 0x007) << 11);  // d maps to Bit 13..11.
+    packed |= ((e & 0x3ff) <<  1);  // e maps to Bit 10..1.
+    return packed;
+}
+
+std::string OmniFirewire::unpackSerialNumber(uint32_t packed)
+{
+    // Unpack the variables based on the following rule:
+    //     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    //     |1|0|0|0|0|0|  a  |   b   |    c    |  d  |         e         |1|
+    //     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    //     |31           24|23           16|15            8|7             0|
+
+    uint32_t a = (packed >> 23) & 0x007;
+    uint32_t b = (packed >> 19) & 0x00f;
+    uint32_t c = (packed >> 14) & 0x01f;
+    uint32_t d = (packed >> 11) & 0x007;
+    uint32_t e = (packed >>  1) & 0x3ff;
+
+    std::stringstream unpacked;
+    unpacked << std::setfill('0');
+    unpacked << std::setw(1) << a;
+    unpacked << std::setw(2) << b;
+    unpacked << std::setw(2) << c;
+    unpacked << std::setw(1) << d;
+    unpacked << std::setw(5) << e;
+    return unpacked.str();
+}
+
+bool OmniFirewire::startIsochronousTransmission()
+{
+    // Make sure raw1394 is ready.
+    if (handle_ == NULL) {
+        return false;
+    }
+
+    // Stop previous transmission.
+    stopIsochronousTransmission();
+
+    // Find a free isochronous channel number for Tx.
+    for (int ch = 0; ch < 63; ch++) {
+        if (raw1394_channel_modify(handle_, ch, RAW1394_MODIFY_ALLOC) == 0) {
+            tx_iso_channel_ = ch;
+            break;
+        }
+    }
+    if (tx_iso_channel_ == -1) {
+        // Channel not found. Abort.
+        stopIsochronousTransmission();
+        return false;
+    }
+
+    // Find a free isochronous channel number for Tx.
+    for (int ch = 0; ch < 63; ch++) {
+        if (raw1394_channel_modify(handle_, ch, RAW1394_MODIFY_ALLOC) == 0) {
+            rx_iso_channel_ = ch;
+            break;
+        }
+    }
+    if (rx_iso_channel_ == -1) {
+        // Channel not found. Abort.
+        stopIsochronousTransmission();
+        return false;
+    }
+
+    // Set the Tx isochronous channel.
+    raw1394_write(handle_, node_, 0x1000, 1, (quadlet_t*)&tx_iso_channel_);
+
+    // Set the Rx isochronous channel.
+    raw1394_write(handle_, node_, 0x1001, 1, (quadlet_t*)&rx_iso_channel_);
+
+    // Not sure what this does, but it is required.
+    {
+        uint32_t data = 0xf80f0000;
+        raw1394_write(handle_, node_, 0x20010, 4, (quadlet_t*)&data);
+    }
+
+    // Tell Omni to start isochronous data transmission.
+    {
+        uint8_t data = 0x08;
+        raw1394_write(handle_, node_, 0x1087, 1, (quadlet_t*)&data);
+    }
+
+    // Register the read (rx) callback that will be called by the driver
+    rx_handle_ = raw1394_new_handle_on_port(port_);
+    raw1394_set_userdata(rx_handle_, this);
+    if (raw1394_iso_recv_init(rx_handle_,
+                              OmniFirewire::callbackRead,
+                              20,
+                              0x40,
+                              rx_iso_channel_,
+                              RAW1394_DMA_PACKET_PER_BUFFER,
+                             -1) == -1)
+    {
+        stopIsochronousTransmission();
+        return false;
+    }
+
+    if (raw1394_iso_recv_start(rx_handle_, -1, -1, 0) == -1)
+    {
+        stopIsochronousTransmission();
+        return false;
+    }
+
+    // Register the write (tx) callback that will be called by the driver
+    tx_handle_ = raw1394_new_handle_on_port(port_);
+    raw1394_set_userdata(tx_handle_, this);
+    if (raw1394_iso_xmit_init(tx_handle_,
+                              OmniFirewire::callbackWrite,
+                              1,
+                              sizeof(tx_iso_buffer_),
+                              tx_iso_channel_,
+                              RAW1394_ISO_SPEED_100,
+                             -1) == -1)
+    {
+        stopIsochronousTransmission();
+        return false;
+    }
+
+    if (raw1394_iso_xmit_start(tx_handle_, -1, -1) == -1)
+    {
+        stopIsochronousTransmission();
+        return false;
+    }
+
+    // Create iso thread
+    thread_driver = ThreadPtr( new boost::thread( boost::bind(&OmniFirewire::isoThreadCallback, this) ) );
+    thread_status = THREAD_STARTED;
+
+    return true;
+}
+
+void OmniFirewire::stopIsochronousTransmission()
+{
+    // Stop the thread.
+    if (thread_status != THREAD_READY) {
+        thread_status = THREAD_TERMINATE;
+        thread_driver->join();
+    }
+    thread_status = THREAD_READY;
+
+    // Make sure raw1394 is ready.
+    if (handle_ == NULL) {
+        return;
+    }
+
+    // Tell Omni to stop isochronous transmission.
+    uint8_t data = 0x00;
+    raw1394_write(handle_, node_, 0x1087, 1, (quadlet_t*)&data);
+
+    if (tx_iso_channel_ != -1) {
+        raw1394_channel_modify(handle_, tx_iso_channel_, RAW1394_MODIFY_FREE);
+        tx_iso_channel_ = -1;
+    }
+
+    if (rx_iso_channel_ != -1) {
+        raw1394_channel_modify(handle_, rx_iso_channel_, RAW1394_MODIFY_FREE);
+        rx_iso_channel_ = -1;
+    }
+
+    if (tx_handle_ != NULL) {
+        raw1394_destroy_handle(tx_handle_);
+        tx_handle_ = NULL;
+    }
+
+    if (rx_handle_ != NULL) {
+        raw1394_destroy_handle(rx_handle_);
+        rx_handle_ = NULL;
+    }
+}
+
+void * OmniFirewire::isoThreadCallback()
+{
+    while (this->thread_status != THREAD_TERMINATE
+           && raw1394_loop_iterate(this->rx_handle_) != -1
+           && raw1394_loop_iterate(this->tx_handle_) != -1)
+    {
+        // Empty
+    }
+
+    return NULL;
+}
+
+std::vector<OmniFirewire::OmniInfo> OmniFirewire::enumerate_omnis()
+{
+    std::vector<OmniInfo> omnis;
+    raw1394handle_t handle = raw1394_new_handle();
+
+    // Get information about the available ports.
+    const int maxports = 5;
+    struct raw1394_portinfo portinfo[maxports];
+    int n_ports = raw1394_get_port_info(handle, portinfo, maxports);
+
+    // Iterate through the list of ports.
+    for (int p = 0 ; p < n_ports; p++) {
+        // Open the port if we can.
+        if (portinfo[p].nodes == 0 || raw1394_set_port(handle, p) == -1) {
+            // Not the port we're interested in.
+            continue;
+        }
+
+        // Iterate through the list of nodes attached to the port.
+        for (int n = 0; n < portinfo[p].nodes; n++) {
+            // Compute the node ID. See IEEE 1394 spec for details.
+            uint32_t node = (1023 << 6) | n;
+
+            // Check the device ID.
+            quadlet_t dev_id = 0;
+            raw1394_read(handle, node, 0x1006000c, sizeof(dev_id), &dev_id);
+            dev_id = ntohl(dev_id);
+            if (dev_id != 0x000b9900) {
+                // Not Phantom Omni.
+                continue;
+            }
+
+            // Now we know it's Phantom Omni. Store the info.
+            quadlet_t serial = 0;
+            raw1394_read(handle, node, 0x10060010, sizeof(serial), &serial);
+            const std::string& unpacked_serial = OmniFirewire::unpackSerialNumber(ntohl(serial));
+            omnis.push_back(OmniInfo(unpacked_serial, p, node));
+        }
+    }
+
+    return omnis;
+}
